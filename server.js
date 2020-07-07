312f64b3239c7ba63df2349e0f8ab0ac85759088
Abhinav Singh <singhabhinav9051571833@gmail.com>
2020-07-07 22:40:11 +0530

    s

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app=express();
 
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.use(express.static(path.join(__dirname,'frontend','build')));
 
 
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/frontend/build/index.html'));
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','build','index.html'));
 })
 
 app.listen(process.env.PORT || 3000);
